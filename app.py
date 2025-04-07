d872a7f4b15e5e52c83aa2920d4ffd15cb66403d
Auduj <dujeuxaurelien97@gmail.com>
2025-04-07 11:45:12 +0200

    Déplacement de index.html à la racine pour GitHub Pages

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from twitchio.ext import commands
 from flask import Flask, jsonify, render_template
 from token_manager import get_valid_token
@@ -15,7 +14,7 @@ participants = {}
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return ('index.html')
 
 @app.route('/participants')
 def get_participants():
@@ -55,62 +54,3 @@ if __name__ == '__main__':
     flask_thread.start()
 
     bot = Bot()
-=======
-from twitchio.ext import commands
-from flask import Flask, jsonify, render_template
-from token_manager import get_valid_token
-import threading
-
-# Remplace ces valeurs par les tiennes
-TOKEN = get_valid_token()
-NICK = 'auduj08'
-CHANNEL = 'Yendo_Jr'
-
-# --- Flask ---
-app = Flask(__name__)
-participants = {}
-
-@app.route('/')
-def index():
-    return render_template('index.html')
-
-@app.route('/participants')
-def get_participants():
-    return jsonify(participants)
-
-# --- Twitch Bot ---
-class Bot(commands.Bot):
-    def __init__(self):
-        super().__init__(token=TOKEN, prefix='!', initial_channels=[CHANNEL])
-
-    async def event_ready(self):
-        print(f'✅ Bot connecté en tant que {self.nick}')
-
-    async def event_message(self, message):
-        if message.echo:
-            return
-        await self.handle_commands(message)
-
-    @commands.command(name='coachme')
-    async def coachme(self, ctx):
-        parts = ctx.message.content.strip().split()
-        if len(parts) == 2:
-            game_id = parts[1]
-            user = ctx.author.name
-            participants[user] = game_id
-            await ctx.send(f"{user} a été ajouté à la liste avec l'ID de game {game_id} !")
-        else:
-            await ctx.send("Utilisation : !coachme ID_GAME")
-
-# --- Thread Flask ---
-def run_flask():
-    app.run(host='0.0.0.0', port=5000)
-
-# --- Lancer ---
-if __name__ == '__main__':
-    flask_thread = threading.Thread(target=run_flask)
-    flask_thread.start()
-
-    bot = Bot()
->>>>>>> 09888a5ec1ede9362a09715ebac9a5232bdade66
-    bot.run()
